037e189123222a4191457ce8449891e55a6c8ff4
Anton Shchukin <a.a.shchukin@gmail.com>
2025-07-24 22:12:16 +0700

    reasons

---
+++ legacy/default_0cab816e653125923bddca528b211da2_v1.js
@@ -0,0 +1,7 @@
+
+; /* Start:"a:4:{s:4:"full";s:95:"/local/templates/aspro-lite/components/bitrix/subscribe.edit/footer/script.min.js?1736450040134";s:6:"source";s:77:"/local/templates/aspro-lite/components/bitrix/subscribe.edit/footer/script.js";s:3:"min";s:81:"/local/templates/aspro-lite/components/bitrix/subscribe.edit/footer/script.min.js";s:3:"map";s:85:"/local/templates/aspro-lite/components/bitrix/subscribe.edit/footer/script.min.js.map";}"*/
+$(document).ready((function(){$("form.subscribe-form").validate({rules:{EMAIL:{email:!0}}})}));
+/* End */
+;; /* /local/templates/aspro-lite/components/bitrix/subscribe.edit/footer/script.min.js?1736450040134*/
+
+//# sourceMappingURL=default_0cab816e653125923bddca528b211da2.map.js
