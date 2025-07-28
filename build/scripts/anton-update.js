515c26681f0b5b70af5dede6a628dac166b24e0d
Anton Shchukin <a.a.shchukin@gmail.com>
2025-07-28 22:40:18 +0700

    clean up

--- build/scripts/anton-update.js
+++ build/scripts/anton-update.js
@@ -20,5 +20,35 @@
   });
 
 
+  /* Swiper */
+  document.querySelectorAll('.swiper--js-init-feedback').forEach((element, index) => {
+    new Swiper(element, {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      autoHeight: true,
+      spaceBetween: 24,
+      pagination: {
+        el: element.querySelector('.swiper-pagination'),
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: element.querySelector('.swiper-button-next'),
+        prevEl: element.querySelector('.swiper-button-prev'),
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          autoHeight: false,
+        },
+        992: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+      },
+    });
+  });
+
+
 })(jQuery);
 
